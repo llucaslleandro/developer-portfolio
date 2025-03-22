5dac7fe66dfab7200d7d3770bb77d1cf12155242
Lucas Leandro <105135633+llucaslleandro@users.noreply.github.com>
2025-03-22 14:42:26 -0300

    update js

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,16 @@
+const acordeonTriggered = document.querySelectorAll('.acordeon .trigger')
 
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
-
-acordeonTriggers.forEach((trigger) => {
+acordeonTriggered.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open')
 
-        if (isOpen) {
+        if(isOpen) {
             acordeon.classList.remove('open')
+
         } else {
             acordeon.classList.add('open')
+        
         }
     })
 })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
@@ -14,51 +13,57 @@ function updateProfileInfo(profileData) {
     location.innerText = profileData.location
 
     const phone = document.getElementById('profile.phone')
+    const cleanedPhone = profileData.phone.replace(/[^\d]/g, '')
     phone.innerText = profileData.phone
-    phone.href = `tel:${profileData.phone}`
+    phone.href = `https://wa.me/+55${cleanedPhone}`
 
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
     email.href = `mailto:${profileData.email}`
+   
 }
 
-function updateSoftSkills(profileData) {
+function updateSoftSkills (profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+   
 }
 
-function updateHardSkills(profileData) {
+function updateHardSkills (profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src='${skill.logo}' alt='${skill.name}' title='${skill.name}'></li>`).join('')
+   
 }
 
-function updateLanguages(profileData) {
+function updateLanguages (profileData) {
     const languages = document.getElementById('profile.languages')
     languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+   
 }
 
-function updatePortfolio(profileData) {
-    const portfolio = document.getElementById('profile.portfolio')
-    portfolio.innerHTML = profileData.portfolio.map(project => {
+
+function updateProjects (profileData) {
+    const projects = document.getElementById('profile.projects')
+    projects.innerHTML = profileData.projects.map(projects => {
         return `
-            <li>
-                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
-                <a href="${project.url}" target="_blank">${project.url}</a>
-            </li>
+        <li>
+            <h3 ${projects.github ? 'class="github"' : ''}>${projects.name}</h3>
+                <a href="${projects.url}" target="_blank">${projects.url}
+                </a>
+        </li>
         `
     }).join('')
-}
+} 
 
-function updateProfessionalExperience(profileData) {
-    const professionalExperience = document.getElementById('profile.professionalExperience')
-    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+function updateExperiences (profileData) {
+    const experiences = document.getElementById('profile.experiences')
+    experiences.innerHTML = profileData.experiences.map(experiences => {
         return `
-            <li>
-                <h3 class="title">${experience.name}</h3>
-                <p class="period">${experience.period}</p>
-                <p>${experience.description}</p>
-            </li>
-        `
+        <li>
+            <h3 class="title">${experiences.name}</h3>
+                <p class="period">${experiences.period}</p>
+                    <p>${experiences.description}</p>
+        </li>`
     }).join('')
 }
 
@@ -68,6 +73,6 @@ function updateProfessionalExperience(profileData) {
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
-    updatePortfolio(profileData)
-    updateProfessionalExperience(profileData)
+    updateProjects(profileData)
+    updateExperiences(profileData)
 })()
